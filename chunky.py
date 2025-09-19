422069f502a510d12036b569cacad99f29b86e96
olivia-long127 <ol2256@columbia.edu>
2025-09-18 20:54:40 -0400

    dictionary extraction

--- chunky.py
+++ chunky.py
@@ -19,19 +19,60 @@ def doc_grouper(folder_dir):
     return docs
  
 
-# def sectionizer(folder_dir):
-#     docs = doc_grouper(folder_dir)
-#     for txt_file in docs:
-#         for article in txt_file:
+def sectionizer(folder_dir):
+    docs = doc_grouper(folder_dir)
+    articles=[]
+    for txt_file in docs:
+        for article in txt_file: #each array is an entire article
+            reg_str = r"\n(.*?)\n"
+            res = re.findall(reg_str, article, flags=re.S)
+            if len(res) < 3:
+                continue
+            else:
+                articles.append(res)
+    return articles
+
+def dictionary(folder_dir):
+    categorized=[]
+    articles = sectionizer(folder_dir)
+    for a in articles:
+        art = {}
+        key="Subject"
+        art[f"{key}"] = a[0]
+        ind=1
+        while ind<len(a):
+            if len(a[ind].split()) < 3 and a[ind] != "":
+                key = a[ind][0:len(a[ind])-1]
+                check_ind=ind+1
+                if check_ind < len(a) and a[check_ind] != "":
+                    art[f"{key}"] = ""
+                elif check_ind < len(a) and a[check_ind] == "":
+                    ind+=1
+                    continue
+                else:
+                    ind+=1
+                    continue
+            elif a[ind] != "":
+                art[f"{key}"]+= " " + a[ind]
+            ind+=1
+        empty_keys=[k for k, v in art.items() if not v]
+        for k in empty_keys:
+            del art[k]
+        categorized.append(art)
+    with open("categorized.json", 'w', encoding='utf-8') as f:
+        json.dump(categorized, f, ensure_ascii=False, indent=4)
+    return categorized
+        
 
 
-#     sections=[]
-#     for file in os.listdir(folder_dir):
-#         file_path = os.path.join(base_dir, file)
-#         with open(file_path, 'r', encoding="utf-8") as f:
-#             lines=[line.strip() for line in f if line.strip()]
-#             print(lines)
+
+    sections=[]
+    for file in os.listdir(folder_dir):
+        file_path = os.path.join(base_dir, file)
+        with open(file_path, 'r', encoding="utf-8") as f:
+            lines=[line.strip() for line in f if line.strip()]
+            print(lines)
         
 
 if __name__ == "__main__":
-    doc_grouper("./text")
+    dictionary("./text")
