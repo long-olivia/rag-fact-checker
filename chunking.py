df8306a675ce7eb33f9ede0fa81bc04c139af75e
olivia-long127 <ol2256@columbia.edu>
2025-09-24 14:20:24 -0400

    trying to test

--- chunking.py
+++ chunking.py
@@ -1,8 +1,17 @@
 import json
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def chunk():
     chunk_id=0
     result=[]
+    chunk_size=500
+    text_splitter=RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=int(chunk_size/10),
+        add_start_index=True,
+        strip_whitespace=True,
+        separators=["\n\n", "\n", ".", " ", ""]
+    )
     with open("categorized.json", 'r') as f:
         data=json.load(f)
     for article in data:
@@ -13,7 +22,7 @@ def chunk():
             if key=="end":
                 break
             s=article[key]
-            splits = [s[i:i+500] for i in range(0, len(s), 500)]
+            splits=text_splitter.split_text(s)
             for x in splits:
                 chunk_dict={}
                 chunk_dict["ID"]=chunk_id
@@ -25,9 +34,8 @@ def chunk():
             key=next(keys, "end")
 
     with open("chunks.json", 'w') as f:
+        print("hit")
         json.dump(result, f, ensure_ascii=False, indent=4)
-
-            
 
 if __name__ == "__main__":
     chunk()
---
+++ embeddings.py
@@ -0,0 +1,54 @@
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores.utils import DistanceStrategy
+from tqdm import tqdm
+import json
+import numpy as np
+from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+def embed():
+    EMBEDDING_MODEL_NAME="sentence-transformers/all-MiniLM-L6-v2" #smaller model since i'm running this on my cpu first
+
+    with open("chunks.json", 'r', encoding="utf-8") as f:
+        data=json.load(f)
+
+    chunks=[item["Content"] for item in data]
+
+    embedding_model=HuggingFaceEmbeddings(
+        model_name=EMBEDDING_MODEL_NAME,
+        multi_process=True,
+        model_kwargs={"device": "cpu"}, #change to colab once we do the whole dataset
+        encode_kwargs={"normalize_embeddings": True}
+    )
+
+    batch_size=64
+    all_embeddings=[]
+
+    for i in tqdm(range(0, len(chunks), batch_size), desc="Embedding chunks"):
+        batch = chunks[i:i+batch_size]
+        embeddings = embedding_model.embed_documents(batch)
+        all_embeddings.extend(embeddings)
+
+    for i, item in enumerate(data):
+        item["embedding"] = all_embeddings[i]
+
+    with open("embedded_doc.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+    
+    vectorize(data, embedding_model)
+
+def vectorize(data, embedding_model):
+    KNOWLEDGE_VECTOR_DATABASE = FAISS.from_documents(
+        data, embedding_model, distance_strategy=DistanceStrategy.COSINE
+    )
+
+
+
+# KNOWLEDGE_VECTOR_DATABASE = FAISS.from_documents(
+#     docs_processed, embedding_model, distance_strategy=DistanceStrategy.COSINE
+# )
+
+if __name__=="__main__":
+    embed()
